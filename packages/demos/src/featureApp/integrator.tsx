b13fbb692327f5194de541096ad5892efb2759a8
Hemin Johnson <achayan1992@gmail.com>
2023-07-06 17:45:15 +0530

    feat: add integrator

--- packages/demos/src/featureApp/integrator.tsx
+++ packages/demos/src/featureApp/integrator.tsx
@@ -6,20 +6,20 @@ import * as ReactDOM from 'react-dom';
 import '../blueprint-css';
 
 const {featureAppManager} = createFeatureHub('test:integrator', {
-    moduleLoader: loadFederatedModule,
+  moduleLoader: loadFederatedModule,
 });
 
 ReactDOM.render(
-    <FeatureHubContextProvider value={{featureAppManager}}>
-        <FeatureAppLoader
-            featureAppId="test:hello-world-1"
-            src="feature-app-1.federated.js"
-        />
-        <FeatureAppLoader
-            featureAppId="test:hello-world-2"
-            src="feature-app-2.federated.js"
-        />
-    </FeatureHubContextProvider>,
+  <FeatureHubContextProvider value={{featureAppManager}}>
+    <FeatureAppLoader
+      featureAppId="test:hello-world-1"
+      src="feature-app-1.federated.js"
+    />
+    <FeatureAppLoader
+      featureAppId="test:hello-world-2"
+      src="feature-app-2.federated.js"
+    />
+  </FeatureHubContextProvider>,
 
-    document.querySelector('main')
+  document.querySelector('main')
 );
